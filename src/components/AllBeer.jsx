7b01f15a0b764364007ea1fa22d252ab52aa86a0
Jean Jia <jean84646@gmail.com>
2018-09-07 10:22:52 -0700

    AllBeer component finish.

--- src/components/AllBeer.jsx
+++ src/components/AllBeer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import SingleBeer from './SingleBeer';
 
 const state = {
   masterKegList: [
@@ -40,7 +42,7 @@ const state = {
       description: 'India Pale Ale',
       abv:  '7.5%',
       price: '6',
-      remaining: '18'
+      remaining: '10'
     },
     {
       name: '8 Hop',
@@ -64,11 +66,32 @@ function AllBeer(){
       <br/>
       <Link to="/less-than-10">Less Than 10 pints</Link>
       <br/>
-      <Link to="/low-price">Low Price</Link>
+      <Link to="/low-price">Less than $5</Link>
       <br/>
 
       <Switch>
-      
+        <Route exact path='/all-beer' component={() => <SingleBeer
+          'is a loop'
+          name={masterKegList[0].name}
+          brewer={masterKegList[0].brewer}
+          description={masterKegList[0].description}
+          abv={masterKegList[0].abv}
+          price={masterKegList[0].price}
+          remaining={masterKegList[0].remaining} />} />
+        <Route exact path='/less-than-10' component={() => <SingleBeer
+          name={masterKegList[5].name}
+          brewer={masterKegList[5].brewer}
+          description={masterKegList[5].description}
+          abv={masterKegList[5].abv}
+          price={masterKegList[5].price}
+          remaining={masterKegList[5].remaining} />} />
+        <Route exact path='/low-remaining' component={() => <SingleBeer
+          name={masterKegList[2].name}
+          brewer={masterKegList[2].brewer}
+          description={masterKegList[2].description}
+          abv={masterKegList[2].abv}
+          price={masterKegList[2].price}
+          remaining={masterKegList[2].remaining} />} />
       </Switch>
     </div>
   );
