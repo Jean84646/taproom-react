c26baf30c830fd100370a4b9d0f570ddea988c4a
Jean Jia <jean84646@gmail.com>
2018-09-14 12:50:54 -0700

    Edit App and beerList components.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,65 @@
 import React from 'react';
 import bg from './../img/bg1.jpg';
 import Header from './Header';
-import BeerInfo from './BeerInfo';
+import BeerList from './BeerList';
 import { Switch, Route, Link } from 'react-router-dom';
 
-function App(){
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterKegList: [
+        {
+          name: 'Ruby Zozzle',
+          brewer: 'Hi-Wheel',
+          description: 'Sparkling Wine & Grapefruit',
+          abv: '6.8%',
+          price: '7',
+          remaining: '20'
+        },
+        {
+          name: 'Tart N Juicy',
+          brewer: 'Epic',
+          description: 'Sour IPA',
+          abv: '4.5%',
+          price: '6',
+          remaining: '60'
+        },
+        {
+          name: 'Hamm\'s',
+          brewer: 'Miller/Coors',
+          description: 'American Lager',
+          abv: '4.7%',
+          price: '3',
+          remaining: '65'
+        },
+        {
+          name: 'Prismatic',
+          brewer: 'Ninkasi',
+          description: 'Juicy IPA',
+          abv:  '5.9%',
+          price: '6',
+          remaining: '75'
+        },
+        {
+          name: 'Juicy Haze',
+          brewer: 'New Belgium',
+          description: 'India Pale Ale',
+          abv:  '7.5%',
+          price: '6',
+          remaining: '18'
+        },
+        {
+          name: '8 Hop',
+          brewer: 'New Belgium',
+          description: 'Pale Ale',
+          abv:  '5.5%',
+          price: '6',
+          remaining: '58'
+        }
+      ]
+    }
+
   return (
     <div>
       <style jsx>{`
@@ -17,7 +72,9 @@ function App(){
         `}</style>
       <div className="container">
         <Header/>
-        <BeerInfo/>
+        <Switch>
+          <Route exact path='/' render={()=><BeerList beerList={this.state.masterKegList}
+        </Switch>
       </div>
     </div>
   );
--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -2,82 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SingleBeer from './SingleBeer';
 
-const state = {
-  masterKegList: [
-    {
-      name: 'Ruby Zozzle',
-      brewer: 'Hi-Wheel',
-      description: 'Sparkling Wine & Grapefruit',
-      abv: '6.8%',
-      price: '7',
-      remaining: '20'
-    },
-    {
-      name: 'Tart N Juicy',
-      brewer: 'Epic',
-      description: 'Sour IPA',
-      abv: '4.5%',
-      price: '6',
-      remaining: '60'
-    },
-    {
-      name: 'Hamm\'s',
-      brewer: 'Miller/Coors',
-      description: 'American Lager',
-      abv: '4.7%',
-      price: '3',
-      remaining: '65'
-    },
-    {
-      name: 'Prismatic',
-      brewer: 'Ninkasi',
-      description: 'Juicy IPA',
-      abv:  '5.9%',
-      price: '6',
-      remaining: '75'
-    },
-    {
-      name: 'Juicy Haze',
-      brewer: 'New Belgium',
-      description: 'India Pale Ale',
-      abv:  '7.5%',
-      price: '6',
-      remaining: '18'
-    },
-    {
-      name: '8 Hop',
-      brewer: 'New Belgium',
-      description: 'Pale Ale',
-      abv:  '5.5%',
-      price: '6',
-      remaining: '58'
-    }
-  ]
-}
-
 function BeerList(props){
   return (
     <div>
-      {state.masterKegList.map((beer, index) =>
+      {props.beerList.map((beer) =>
         <SingleBeer name={beer.name}
           brewer={beer.brewer}
           description={beer.description}
           abv={beer.abv}
           price={beer.price}
           remaining={beer.remaining}
-          key={index} />
+          key={beer.id} />
       )}
     </div>
   );
 }
 
 BeerList.propTypes = {
-  name: PropTypes.string.isRequired,
-  brewer: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  abv: PropTypes.string.isRequired,
-  price: PropTypes.string.isRequired,
-  remaining: PropTypes.string.isRequired
+  beerList: PropTypes.array.isRequired
 };
 
 export default BeerList;
